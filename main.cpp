e616250542aed11d35d831bb6de4fce6c1af3e4a
MCLEANS <jackagwaya@gmail.com>
2020-08-26 13:04:36 +0300

    added hc05 firmware version querry

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include "stm32f10x.h"
+#include "USART.h"
+
+#define RX_PIN 10
+#define TX_PIN 9
+#define BAUDRATE 38400
+
+
+
+custom_libraries::USART hc05(USART1,
+                            GPIOA,
+                            custom_libraries::_DMA1,
+                            DMA1_Channel5,
+                            RX_PIN,
+                            TX_PIN,
+                            BAUDRATE);
+
+char GET_BLUETOOTH_VERSION[] = "AT+VERSION?r\n";
 
 int main(void) {
 
+    hc05.initialize();
+    //get firmware version
+    hc05.print(GET_BLUETOOTH_VERSION);
+    
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
     GPIOC->CRH |= GPIO_CRH_MODE13;
     GPIOC->CRH &= ~GPIO_CRH_CNF13;
